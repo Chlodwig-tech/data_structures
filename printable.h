104a219596ae406e60ad4054ac81b4420badf4e9
Chlodwig-tech <80710710+Chlodwig-tech@users.noreply.github.com>
2021-07-10 18:18:46 +0200

    Create printable.h

---
+++ printable.h
@@ -0,0 +1,12 @@
+#ifndef PRINTABLE
+#define PRINTABLE
+class Printable{
+public:
+    virtual void __cout__(std::ostream &ostr)=0;
+  
+    friend std::ostream& operator<<(std::ostream& ostr,Printable &printable){
+	      printable.__cout__(ostr);
+        return ostr;
+    }
+};
+#endif // PRINTABLE
