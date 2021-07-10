975e0cbaf9f1befbc9bda1dff1a21c0293aa4330
Chlodwig-tech <80710710+Chlodwig-tech@users.noreply.github.com>
2021-07-10 18:15:03 +0200

    Create List_element.cpp

---
+++ List/List_element.cpp
@@ -0,0 +1,20 @@
+#ifndef LIST_ELEMENT_CPP
+#define LIST_ELEMENT_CPP
+
+#include "list_element.h"
+
+namespace data_structures {
+
+template <typename T> List_element<T>::List_element(T data){
+    this->data=data;
+    this->next=nullptr;
+    this->previous=nullptr;
+}
+
+template <typename T> void List_element<T>::__cout__(std::ostream &ostr){
+    ostr<<data;
+}
+
+}
+
+#endif // LIST_ELEMENT_CPP
