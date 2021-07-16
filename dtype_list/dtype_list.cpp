73b617da79c3951473062d4592ee025b149779a3
Chlodwig-tech <80710710+Chlodwig-tech@users.noreply.github.com>
2021-07-16 15:19:05 +0200

    Update dtype_list.cpp

--- dtype_list/dtype_list.cpp
+++ dtype_list/dtype_list.cpp
@@ -264,6 +264,76 @@ template<typename T> T dtype_list::get(int index){
     return e->data;
 }
 
+void dtype_list::save(std::string path){
+    std::ofstream file(path);
+    file<<"<dtype_list>\n";
+    for(dtype_super_element *helper=first;helper!=nullptr;helper=helper->next){
+        try{
+            int data=type.get(helper);
+            file<<"<int>\n\t"<<data<<"\n</int>\n";
+            continue;
+        }catch(...){}
+        try{
+            float data=type.get(helper);
+            file<<"<float>\n\t"<<data<<"\n</float>\n";
+            continue;
+        }catch(...){}
+        try{
+            double data=type.get(helper);
+            file<<"<double>\n\t"<<data<<"\n</double>\n";            
+            continue;
+        }catch(...){}
+        try{
+            char data=type.get(helper);
+            file<<"<char>\n\t"<<data<<"\n</char>\n";            
+            continue;
+        }catch(...){}
+        try{
+            std::string data=type.get(helper);
+            file<<"<std::string>\n\t"<<data<<"\n</std::string>\n";
+            continue;
+        }catch(...){}
+        try{
+            const char* data=type.get(helper);
+            file<<"<const char*>\n\t"<<data<<"\n</const char*>\n";
+            continue;
+        }catch(...){}
+    }
+    file<<"</dtype_list>\n";
+    file.close();
+}
+
+void dtype_list::read(std::string path){
+    std::ifstream file(path);
+    std::string start;    
+    std::string line;
+    getline(file,start);
+    if(start=="<dtype_list>"){
+        while(getline(file,line)){
+	    if(line=="</dtype_list>"){
+	   	break;
+ 	    }
+ 	    std::string data;
+            getline(file,data);
+            data.erase(data.begin());
+            if(line=="<int>"){
+                push_back(stoi(data));
+            }else if(line=="<float>"){
+                push_back(stof(data));
+            }else if(line=="<double>"){
+                push_back(stod(data));
+            }else if(line=="<char>"){
+                push_back(char(data[0]));
+            }else if(line=="<std::string>"){
+                push_back(data);
+            }else if(line=="<const char*>"){
+                push_back(data);
+            }
+            getline(file,line);
+        }
+    }
+    file.close();
+}
 
 // Operators
 dtype_super_element* dtype_list::operator()(int i){
